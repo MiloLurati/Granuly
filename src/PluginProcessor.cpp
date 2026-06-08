ce0dc7dde5981caca9a01472643e0ba957f31721
Milo Lurati <lurati.milo@gmail.com>
2026-06-08 13:54:21 +0200

    format: full project clang-format

--- src/PluginProcessor.cpp
+++ src/PluginProcessor.cpp
@@ -1,204 +1,184 @@
 #include "PluginProcessor.h"
-#include "PluginEditor.h"
 #include "BinaryData.h"
+#include "PluginEditor.h"
 
 //==============================================================================
 AudioPluginAudioProcessor::AudioPluginAudioProcessor()
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       )
-{
-    // 1. Tell the manager to register basic formats like WAV and AIFF
-    formatManager.registerBasicFormats();
+    : AudioProcessor(
+          BusesProperties()
+#if !JucePlugin_IsMidiEffect
+#if !JucePlugin_IsSynth
+              .withInput("Input", juce::AudioChannelSet::stereo(), true)
+#endif
+              .withOutput("Output", juce::AudioChannelSet::stereo(), true)
+#endif
+      ) {
+  // 1. Tell the manager to register basic formats like WAV and AIFF
+  formatManager.registerBasicFormats();
 
-    // 2. Create a memory stream pointing directly to your embedded binary file
-    auto memoryStream = std::make_unique<juce::MemoryInputStream>(
-        BinaryData::OTH_128_Gm_Forward_Synth_wav,
-        BinaryData::OTH_128_Gm_Forward_Synth_wavSize,
-        false
-    );
+  // 2. Create a memory stream pointing directly to your embedded binary file
+  auto memoryStream = std::make_unique<juce::MemoryInputStream>(
+      BinaryData::OTH_128_Gm_Forward_Synth_wav,
+      BinaryData::OTH_128_Gm_Forward_Synth_wavSize, false);
 
-    // 3. Find a reader that can understand the stream data
-    std::unique_ptr<juce::AudioFormatReader> reader(formatManager.createReaderFor(std::move(memoryStream)));
+  // 3. Find a reader that can understand the stream data
+  std::unique_ptr<juce::AudioFormatReader> reader(
+      formatManager.createReaderFor(std::move(memoryStream)));
 
-    if (reader != nullptr)
-    {
-        // 4. Resize our reservoir buffer to match the audio file channels and length
-        audioReservoir.setSize(reader->numChannels, static_cast<int>(reader->lengthInSamples));
+  if (reader != nullptr) {
+    // 4. Resize our reservoir buffer to match the audio file channels and
+    // length
+    audioReservoir.setSize(reader->numChannels,
+                           static_cast<int>(reader->lengthInSamples));
 
-        // 5. Read the samples from the file stream right into our buffer
-        reader->read(&audioReservoir, 
-                     0,                                // Start at sample 0 in reservoir
-                     static_cast<int>(reader->lengthInSamples), 
-                     0,                                // Start at sample 0 in file
-                     true,                             // Read left channel
-                     true);                            // Read right channel
-        
-        std::cout << "Successfully loaded sample! Length: " << audioReservoir.getNumSamples() << " samples." << std::endl;
-    }
-    else
-    {
-        std::cout << "Failed to read binary audio data!" << std::endl;
-    }
+    // 5. Read the samples from the file stream right into our buffer
+    reader->read(&audioReservoir,
+                 0, // Start at sample 0 in reservoir
+                 static_cast<int>(reader->lengthInSamples),
+                 0,     // Start at sample 0 in file
+                 true,  // Read left channel
+                 true); // Read right channel
 
-    granularEngine = std::make_unique<GranularEngine>(audioReservoir, reader->lengthInSamples, 4410, 0.5f, 50, 882);
+    std::cout << "Successfully loaded sample! Length: "
+              << audioReservoir.getNumSamples() << " samples." << std::endl;
+  } else {
+    std::cout << "Failed to read binary audio data!" << std::endl;
+  }
+
+  granularEngine = std::make_unique<GranularEngine>(
+      audioReservoir, reader->lengthInSamples, 4410, 0.5f, 50, 882);
 }
 
-AudioPluginAudioProcessor::~AudioPluginAudioProcessor()
-{
+AudioPluginAudioProcessor::~AudioPluginAudioProcessor() {}
+
+//==============================================================================
+const juce::String AudioPluginAudioProcessor::getName() const {
+  return JucePlugin_Name;
+}
+
+bool AudioPluginAudioProcessor::acceptsMidi() const {
+#if JucePlugin_WantsMidiInput
+  return true;
+#else
+  return false;
+#endif
+}
+
+bool AudioPluginAudioProcessor::producesMidi() const {
+#if JucePlugin_ProducesMidiOutput
+  return true;
+#else
+  return false;
+#endif
+}
+
+bool AudioPluginAudioProcessor::isMidiEffect() const {
+#if JucePlugin_IsMidiEffect
+  return true;
+#else
+  return false;
+#endif
+}
+
+double AudioPluginAudioProcessor::getTailLengthSeconds() const { return 0.0; }
+
+int AudioPluginAudioProcessor::getNumPrograms() {
+  return 1; // NB: some hosts don't cope very well if you tell them there are 0
+            // programs, so this should be at least 1, even if you're not really
+            // implementing programs.
+}
+
+int AudioPluginAudioProcessor::getCurrentProgram() { return 0; }
+
+void AudioPluginAudioProcessor::setCurrentProgram(int index) {
+  juce::ignoreUnused(index);
+}
+
+const juce::String AudioPluginAudioProcessor::getProgramName(int index) {
+  juce::ignoreUnused(index);
+  return {};
+}
+
+void AudioPluginAudioProcessor::changeProgramName(int index,
+                                                  const juce::String &newName) {
+  juce::ignoreUnused(index, newName);
 }
 
 //==============================================================================
-const juce::String AudioPluginAudioProcessor::getName() const
-{
-    return JucePlugin_Name;
+void AudioPluginAudioProcessor::prepareToPlay(double sampleRate,
+                                              int samplesPerBlock) {}
+
+void AudioPluginAudioProcessor::releaseResources() {
+  // When playback stops, you can use this as an opportunity to free up any
+  // spare memory, etc.
 }
 
-bool AudioPluginAudioProcessor::acceptsMidi() const
-{
-   #if JucePlugin_WantsMidiInput
-    return true;
-   #else
+bool AudioPluginAudioProcessor::isBusesLayoutSupported(
+    const BusesLayout &layouts) const {
+#if JucePlugin_IsMidiEffect
+  juce::ignoreUnused(layouts);
+  return true;
+#else
+  // This is the place where you check if the layout is supported.
+  // In this template code we only support mono or stereo.
+  // Some plugin hosts, such as certain GarageBand versions, will only
+  // load plugins that support stereo bus layouts.
+  if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono() &&
+      layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
     return false;
-   #endif
-}
 
-bool AudioPluginAudioProcessor::producesMidi() const
-{
-   #if JucePlugin_ProducesMidiOutput
-    return true;
-   #else
+  // This checks if the input layout matches the output layout
+#if !JucePlugin_IsSynth
+  if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
     return false;
-   #endif
+#endif
+
+  return true;
+#endif
 }
 
-bool AudioPluginAudioProcessor::isMidiEffect() const
-{
-   #if JucePlugin_IsMidiEffect
-    return true;
-   #else
-    return false;
-   #endif
-}
+void AudioPluginAudioProcessor::processBlock(juce::AudioBuffer<float> &buffer,
+                                             juce::MidiBuffer &midiMessages) {
+  juce::ScopedNoDenormals noDenormals;
+  auto totalNumInputChannels = getTotalNumInputChannels();
+  auto totalNumOutputChannels = getTotalNumOutputChannels();
+  int bufferNumSamples = buffer.getNumSamples();
 
-double AudioPluginAudioProcessor::getTailLengthSeconds() const
-{
-    return 0.0;
-}
+  for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i) {
+    buffer.clear(i, 0, bufferNumSamples);
+  }
 
-int AudioPluginAudioProcessor::getNumPrograms()
-{
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
-}
-
-int AudioPluginAudioProcessor::getCurrentProgram()
-{
-    return 0;
-}
-
-void AudioPluginAudioProcessor::setCurrentProgram (int index)
-{
-    juce::ignoreUnused (index);
-}
-
-const juce::String AudioPluginAudioProcessor::getProgramName (int index)
-{
-    juce::ignoreUnused (index);
-    return {};
-}
-
-void AudioPluginAudioProcessor::changeProgramName (int index, const juce::String& newName)
-{
-    juce::ignoreUnused (index, newName);
+  granularEngine->processBlock(buffer);
 }
 
 //==============================================================================
-void AudioPluginAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
-{
+bool AudioPluginAudioProcessor::hasEditor() const {
+  return true; // (change this to false if you choose to not supply an editor)
 }
 
-void AudioPluginAudioProcessor::releaseResources()
-{
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
-}
-
-bool AudioPluginAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
-{
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
-    return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    // Some plugin hosts, such as certain GarageBand versions, will only
-    // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-        return false;
-
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
-   #endif
-
-    return true;
-  #endif
-}
-
-void AudioPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
-                                              juce::MidiBuffer& midiMessages)
-{
-    juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
-    int bufferNumSamples = buffer.getNumSamples();
-
-    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-    {
-        buffer.clear(i, 0, bufferNumSamples);
-    }
-
-    granularEngine->processBlock(buffer);
+juce::AudioProcessorEditor *AudioPluginAudioProcessor::createEditor() {
+  return new AudioPluginAudioProcessorEditor(*this);
 }
 
 //==============================================================================
-bool AudioPluginAudioProcessor::hasEditor() const
-{
-    return true; // (change this to false if you choose to not supply an editor)
+void AudioPluginAudioProcessor::getStateInformation(
+    juce::MemoryBlock &destData) {
+  // You should use this method to store your parameters in the memory block.
+  // You could do that either as raw data, or use the XML or ValueTree classes
+  // as intermediaries to make it easy to save and load complex data.
+  juce::ignoreUnused(destData);
 }
 
-juce::AudioProcessorEditor* AudioPluginAudioProcessor::createEditor()
-{
-    return new AudioPluginAudioProcessorEditor (*this);
-}
-
-//==============================================================================
-void AudioPluginAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
-{
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
-    juce::ignoreUnused (destData);
-}
-
-void AudioPluginAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
-{
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
-    juce::ignoreUnused (data, sizeInBytes);
+void AudioPluginAudioProcessor::setStateInformation(const void *data,
+                                                    int sizeInBytes) {
+  // You should use this method to restore your parameters from this memory
+  // block, whose contents will have been created by the getStateInformation()
+  // call.
+  juce::ignoreUnused(data, sizeInBytes);
 }
 
 //==============================================================================
 // This creates new instances of the plugin..
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
-{
-    return new AudioPluginAudioProcessor();
+juce::AudioProcessor *JUCE_CALLTYPE createPluginFilter() {
+  return new AudioPluginAudioProcessor();
 }
--- src/PluginProcessor.h
+++ src/PluginProcessor.h
@@ -1,55 +1,57 @@
 #pragma once
 
+#include <juce_audio_formats/juce_audio_formats.h>
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_dsp/juce_dsp.h>
-#include <juce_audio_formats/juce_audio_formats.h>
 
 #include "GranularEngine.h"
 
 //==============================================================================
-class AudioPluginAudioProcessor final : public juce::AudioProcessor
-{
+class AudioPluginAudioProcessor final : public juce::AudioProcessor {
 public:
-    //==============================================================================
-    AudioPluginAudioProcessor();
-    ~AudioPluginAudioProcessor() override;
+  //==============================================================================
+  AudioPluginAudioProcessor();
+  ~AudioPluginAudioProcessor() override;
 
-    //==============================================================================
-    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
-    void releaseResources() override;
+  //==============================================================================
+  void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+  void releaseResources() override;
 
-    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+  bool isBusesLayoutSupported(const BusesLayout &layouts) const override;
 
-    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
-    using AudioProcessor::processBlock;
+  void processBlock(juce::AudioBuffer<float> &, juce::MidiBuffer &) override;
+  using AudioProcessor::processBlock;
 
-    //==============================================================================
-    juce::AudioProcessorEditor* createEditor() override;
-    bool hasEditor() const override;
+  //==============================================================================
+  juce::AudioProcessorEditor *createEditor() override;
+  bool hasEditor() const override;
 
-    //==============================================================================
-    const juce::String getName() const override;
+  //==============================================================================
+  const juce::String getName() const override;
 
-    bool acceptsMidi() const override;
-    bool producesMidi() const override;
-    bool isMidiEffect() const override;
-    double getTailLengthSeconds() const override;
+  bool acceptsMidi() const override;
+  bool producesMidi() const override;
+  bool isMidiEffect() const override;
+  double getTailLengthSeconds() const override;
 
-    //==============================================================================
-    int getNumPrograms() override;
-    int getCurrentProgram() override;
-    void setCurrentProgram (int index) override;
-    const juce::String getProgramName (int index) override;
-    void changeProgramName (int index, const juce::String& newName) override;
+  //==============================================================================
+  int getNumPrograms() override;
+  int getCurrentProgram() override;
+  void setCurrentProgram(int index) override;
+  const juce::String getProgramName(int index) override;
+  void changeProgramName(int index, const juce::String &newName) override;
 
-    //==============================================================================
-    void getStateInformation (juce::MemoryBlock& destData) override;
-    void setStateInformation (const void* data, int sizeInBytes) override;
+  //==============================================================================
+  void getStateInformation(juce::MemoryBlock &destData) override;
+  void setStateInformation(const void *data, int sizeInBytes) override;
 
 private:
-    //==============================================================================
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessor)
-    juce::AudioBuffer<float> audioReservoir;
-    juce::AudioFormatManager formatManager;
-    std::unique_ptr<GranularEngine> granularEngine;
+  //==============================================================================
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessor)
+  juce::AudioBuffer<float> audioReservoir;
+  juce::AudioFormatManager formatManager;
+  std::unique_ptr<GranularEngine> granularEngine;
+
+  juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+  juce::AudioProcessorValueTreeState apvts;
 };
